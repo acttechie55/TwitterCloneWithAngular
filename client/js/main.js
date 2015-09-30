9c88081764dd1a09a60455106314ad08d984ea62
acttechie55 <actaylor55@gmail.com>
2015-09-29 19:33:03 -0500

    angular draft 2

--- client/js/main.js
+++ client/js/main.js
@@ -1,8 +1,45 @@
-angular.module('myApp', [])
-	.controller('MyController', ['', function(){
+/*This is the Code-School Method (Module and Controller on same page)*/
 
+var myApp = angular.module('myApp', ['ngRoute']);
+	myApp.controller('TweetsController', ['$scope', '$http', function($scope, $http){
+		//$scope.tweets = [];
+		//$scope.userName = 'Afftene';
+		$http.get('/messages')
+			.success(function(tweets) {
+				$scope.tweets = tweets.reverse();
+			})
+			.error(function(err) {
+				console.error(err);
+			});
+		$scope.postTweet = function(){
+			var tweet = {
+				text: $scope.tweet,
+				user: $scope.tweetUser
+			};
+			$http.post('/messages', tweet)
+			.success(function() {
+				$http.get('/messages')
+			.success(function (messages) {
+				$scope.tweets = messages.reverse();
+			})
+			.error(function(err) {
+				console.error(err);
+			});
+		})
+		}
 	}])
-	.config(['', function(){
-
-
-}]);
+	
+	myApp.config(function($routeProvider){
+		$routeProvider
+		.when('/login', {
+			//controller: 'WelcomeController',
+			templateUrl: '../views/welcome.html'
+		})
+		.when('/mytweets', {
+			controller: 'TweetsController',
+			templateUrl: '../views/tweets.html'
+		})
+		.otherwise ({
+			redirectTo: '/login'
+		});
+});
