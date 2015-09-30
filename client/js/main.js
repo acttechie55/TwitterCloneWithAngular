a4205780d3c629ff88a1675b768f4a51a1042b90
acttechie55 <actaylor55@gmail.com>
2015-09-30 12:54:13 -0500

    Completed Twitter Clone

--- client/js/main.js
+++ client/js/main.js
@@ -1,9 +1,9 @@
 /*This is the Code-School Method (Module and Controller on same page)*/
 
 var myApp = angular.module('myApp', ['ngRoute']);
-	myApp.controller('TweetsController', ['$scope', '$http', function($scope, $http){
+	myApp.controller('TweetsController', ['$scope', '$http', '$rootScope', function($scope, $http, $rootScope){
 		//$scope.tweets = [];
-		//$scope.userName = 'Afftene';
+		$scope.userName = $rootScope.loggedInUser;
 		$http.get('/messages')
 			.success(function(tweets) {
 				$scope.tweets = tweets.reverse();
@@ -32,7 +32,7 @@ var myApp = angular.module('myApp', ['ngRoute']);
 	myApp.config(function($routeProvider){
 		$routeProvider
 		.when('/login', {
-			//controller: 'WelcomeController',
+			controller: 'WelcomeController',
 			templateUrl: '../views/welcome.html'
 		})
 		.when('/mytweets', {
@@ -42,4 +42,14 @@ var myApp = angular.module('myApp', ['ngRoute']);
 		.otherwise ({
 			redirectTo: '/login'
 		});
-});
+	});
+		
+//Creating a Welcome Controller//
+myApp.controller('WelcomeController', ['$scope', '$location', '$rootScope', function($scope, $location, $rootScope) {
+//Attempting to get my log-in button to click to my tweets page//	
+	$scope.login = function(){
+		$rootScope.loggedInUser = $scope.loginName;
+		$location.path('/mytweets');
+	};
+
+}]);
